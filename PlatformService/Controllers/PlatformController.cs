c80717625e713232a2c21b9acceb1bf47a0c2a5f
Fabrício Docema <fa.docema@gmail.com>
2023-04-25 21:05:18 -0300

    Added Controller Http Get/Post.Correction on DTOs

--- PlatformService/Controllers/PlatformController.cs
+++ PlatformService/Controllers/PlatformController.cs
@@ -1,7 +1,51 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.Dtos;
+using PlatformService.Models;
+
 namespace PlatformService.Controllers
 {
-    public class PlatformController 
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformController : ControllerBase
     {
+        private readonly IPlatformRepository _repository;
+        private readonly IMapper _mapper;
 
+        public PlatformController(IPlatformRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            Console.WriteLine("Get Platforms.");
+            var platformItems = _repository.GetAllPlatforms();
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItems));
+        }
+
+        [HttpGet("{id}", Name = "GetPlatformByID")]
+        public ActionResult<PlatformReadDto> GetPlatformByID(int id)
+        {
+            var platformItem = _repository.GetPlatformById(id);
+            if(platformItem != null)
+            {
+                return Ok(_mapper.Map<PlatformReadDto>(platformItem));
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platformCreateDto)
+        {
+            var platformModel = _mapper.Map<Platform>(platformCreateDto);
+            _repository.CreatePlatform(platformModel);
+            _repository.SaveChanges();
+            var platformReadDto = _mapper.Map<PlatformReadDto>(platformModel);
+            return CreatedAtRoute(nameof(GetPlatformByID), new {Id = platformReadDto.Id }, platformReadDto);
+        }
     }
 }
--- PlatformService/Dtos/PlatformReadDto.cs
+++ PlatformService/Dtos/PlatformReadDto.cs
@@ -1,14 +1,12 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace PlatformService.Dtos
 {
     public class PlatformReadDto
     {
-        [Required]
+        public int Id { get; set; }
         public string Name { get; set; } = "";
-        [Required]
+
         public string Publisher { get; set; } = "";
-        [Required]
+
         public string Cost { get; set; } = "";
     }
 }
